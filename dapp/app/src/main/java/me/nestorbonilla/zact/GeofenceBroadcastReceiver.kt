21ab65281ae7c1655e0c20ffbd2bb32f8c96ddfa
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2020-06-24 21:10:36 -0500

    geofence functionality added

---
+++ dapp/app/src/main/java/me/nestorbonilla/zact/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,14 @@
+package me.nestorbonilla.zact
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        Toast.makeText(context, "Geofence triggered", Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ dapp/app/src/main/java/me/nestorbonilla/zact/GeofenceHelper.kt
@@ -0,0 +1,61 @@
+package me.nestorbonilla.zact
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.util.Log
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.maps.model.LatLng
+import java.lang.Exception
+
+open class GeofenceHelper(base: Context?) : ContextWrapper(base) {
+
+    private var TAG = "GeofenceHelper"
+    //private lateinit var pendingIntent: PendingIntent
+
+    fun getGeofencingRequest(geofence: Geofence): GeofencingRequest {
+        return GeofencingRequest.Builder()
+            .addGeofence(geofence)
+            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            .build()
+    }
+
+    fun getGeofence(ID: String, latLng: LatLng, radius: Float, transitionTypes: Int): Geofence {
+        return Geofence.Builder()
+            .setCircularRegion(latLng.latitude, latLng.longitude, radius)
+            .setRequestId(ID)
+            .setTransitionTypes(transitionTypes)
+            .setLoiteringDelay(5000)
+            .setExpirationDuration(Geofence.NEVER_EXPIRE)
+            .build()
+    }
+
+    fun getPendingIntent(): PendingIntent {
+        //if(pendingIntent != null) {
+        //    Log.d(TAG, "pendingIntent not null")
+        //    return pendingIntent
+        //} else {
+            Log.d(TAG, "creating pendingIntent")
+            val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
+            var pendingIntent: PendingIntent = PendingIntent.getBroadcast(this, 2607, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            return pendingIntent
+        //}
+    }
+
+    fun getErrorString(e: Exception): String {
+        if (e is ApiException) run {
+            var apiException: ApiException = e
+            when (apiException.statusCode) {
+                GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE -> return "GEOFENCE_NOT_AVAILABLE"
+                GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES -> return "GEOFENCE_TOO_MANY_GEOFENCES"
+                GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS -> return "GEOFENCE_TOO_MANY_PENDING_INTENTS"
+                else -> return "GEOFENCE_ERROR_UNKNOWN"
+            }
+        }
+        return e.localizedMessage
+    }
+}
