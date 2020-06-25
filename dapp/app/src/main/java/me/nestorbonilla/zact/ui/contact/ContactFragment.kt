94d2e10eb4b8a93105a1b7760f33ad5457dba7fe
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2020-06-25 17:42:37 -0500

    bottom tabs added

---
+++ dapp/app/src/main/java/me/nestorbonilla/zact/ui/contact/ContactFragment.kt
@@ -0,0 +1,31 @@
+package me.nestorbonilla.zact.ui.contact
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import me.nestorbonilla.zact.R
+
+class ContactFragment : Fragment() {
+
+    private lateinit var contactViewModel: ContactViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        contactViewModel =
+            ViewModelProviders.of(this).get(ContactViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_contact, container, false)
+        val textView: TextView = root.findViewById(R.id.text_contact)
+        contactViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ dapp/app/src/main/java/me/nestorbonilla/zact/ui/contact/ContactViewModel.kt
@@ -0,0 +1,13 @@
+package me.nestorbonilla.zact.ui.contact
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is contact Fragment"
+    }
+    val text: LiveData<String> = _text
+}
